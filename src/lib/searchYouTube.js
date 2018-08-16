96f425b89b67e3a854e100a34dc6c9eda1df5d24
charliegphan <charliegphan@gmail.com>
2018-08-16 12:39:59 -0700

    implement fetch request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,22 +1,42 @@
-var searchYouTube = ({key, query, max = 5}, callback) => {
-  $.get('https://www.googleapis.com/youtube/v3/search', {
+// var searchYouTube = ({key, query, max = 5}, callback) => {
+//   $.get('https://www.googleapis.com/youtube/v3/search', {
+//     part: 'snippet',
+//     key: key,
+//     q: query,
+//     maxResults: max,
+//     type: 'video',
+//     videoEmbeddable: 'true'
+//   })
+//     .done(({items}) => {
+//       if (callback) {
+//         callback(items);
+//       }
+//     })
+//     .fail(({responseJSON}) => {
+//       responseJSON.error.errors.forEach((err) =>
+//         console.error(err)
+//       );
+//     });
+// };
+
+var searchYouTube2 = ({key, query, max = 5}, callback) => {
+  var esc = encodeURIComponent;
+  var data = {
     part: 'snippet',
     key: key,
     q: query,
     maxResults: max,
     type: 'video',
     videoEmbeddable: 'true'
-  })
-    .done(({items}) => {
-      if (callback) {
-        callback(items);
-      }
-    })
-    .fail(({responseJSON}) => {
-      responseJSON.error.errors.forEach((err) =>
-        console.error(err)
-      );
-    });
-};
+  }
+  var query = Object.keys(data)
+    .map(k => esc(k) + '=' + esc(data[k]))
+    .join('&');
 
-export default searchYouTube;
+  fetch(`https://www.googleapis.com/youtube/v3/search?${query}`)
+    .then(response => response.json())
+    .then(data => data.items)
+    .then(items => callback(items))
+}
+
+export default searchYouTube2;
